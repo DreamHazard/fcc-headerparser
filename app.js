96ae2e5de97fd8620635f0c79dc6f37c6e397123
HADES\xander <xander@dreamhazard.com>
2018-01-25 22:00:10 +0000

    staged initial app to test on Glitch

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var ua = require('express-useragent')
+
+//set up port for use on hosted or local systems
+const port = process.env.PORT || 3000;
+
+//initialise modules to be used within the app
+var app = module.imports = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(ua.express());
+
+app.get('/', function(req, res, next){
+  var lang = req.acceptsLanguages();
+  var soft = "OS: " + req.useragent.os;
+  soft += ", Browser: " + req.useragent.browser;
+  var ip = req.ip;
+
+  res.json({'ip':ip, 'lang':lang[0], 'soft':soft});
+})
+
+
+//start listen server
+app.listen(port, function(){
+});
